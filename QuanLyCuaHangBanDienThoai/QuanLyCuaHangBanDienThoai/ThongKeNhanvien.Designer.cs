5f55a856851917384425992356d7c3cdfb793571
TranThuPhuong <thuphuongtran767@gmail.com>
2022-03-31 22:38:02 +0700

    SXTKNCtheoNam.rpt && Dataset 5 mat ô tháng

--- QuanLyCuaHangBanDienThoai/QuanLyCuaHangBanDienThoai/ThongKeNhanvien.Designer.cs
+++ QuanLyCuaHangBanDienThoai/QuanLyCuaHangBanDienThoai/ThongKeNhanvien.Designer.cs
@@ -38,7 +38,19 @@ namespace QuanLyCuaHangBanDienThoai
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbNam = new System.Windows.Forms.ComboBox();
+            this.cbThang = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbMax = new System.Windows.Forms.RadioButton();
+            this.rbTang = new System.Windows.Forms.RadioButton();
+            this.rbMin = new System.Windows.Forms.RadioButton();
+            this.rbGiam = new System.Windows.Forms.RadioButton();
+            this.cbLoaiSX = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbTuoitu
@@ -57,9 +69,9 @@ namespace QuanLyCuaHangBanDienThoai
             | System.Windows.Forms.AnchorStyles.Right)));
             this.crvNV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crvNV.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crvNV.Location = new System.Drawing.Point(12, 74);
+            this.crvNV.Location = new System.Drawing.Point(12, 183);
             this.crvNV.Name = "crvNV";
-            this.crvNV.Size = new System.Drawing.Size(776, 378);
+            this.crvNV.Size = new System.Drawing.Size(776, 269);
             this.crvNV.TabIndex = 1;
             // 
             // btnHien
@@ -106,11 +118,153 @@ namespace QuanLyCuaHangBanDienThoai
             this.label2.TabIndex = 5;
             this.label2.Text = "Tuổi đến:";
             // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Tháng";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 78);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Năm";
+            // 
+            // cbNam
+            // 
+            this.cbNam.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cbNam.FormattingEnabled = true;
+            this.cbNam.Location = new System.Drawing.Point(63, 76);
+            this.cbNam.Name = "cbNam";
+            this.cbNam.Size = new System.Drawing.Size(121, 21);
+            this.cbNam.TabIndex = 13;
+            // 
+            // cbThang
+            // 
+            this.cbThang.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cbThang.FormattingEnabled = true;
+            this.cbThang.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.cbThang.Location = new System.Drawing.Point(63, 105);
+            this.cbThang.Name = "cbThang";
+            this.cbThang.Size = new System.Drawing.Size(121, 21);
+            this.cbThang.TabIndex = 16;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.rbMax);
+            this.groupBox2.Controls.Add(this.rbTang);
+            this.groupBox2.Controls.Add(this.rbMin);
+            this.groupBox2.Controls.Add(this.rbGiam);
+            this.groupBox2.Controls.Add(this.cbLoaiSX);
+            this.groupBox2.Location = new System.Drawing.Point(216, 76);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 101);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sắp xếp";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 19);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Loại";
+            // 
+            // rbMax
+            // 
+            this.rbMax.AutoSize = true;
+            this.rbMax.Enabled = false;
+            this.rbMax.Location = new System.Drawing.Point(16, 74);
+            this.rbMax.Name = "rbMax";
+            this.rbMax.Size = new System.Drawing.Size(68, 17);
+            this.rbMax.TabIndex = 6;
+            this.rbMax.TabStop = true;
+            this.rbMax.Text = "Cao nhất";
+            this.rbMax.UseVisualStyleBackColor = true;
+            // 
+            // rbTang
+            // 
+            this.rbTang.AutoSize = true;
+            this.rbTang.Enabled = false;
+            this.rbTang.Location = new System.Drawing.Point(16, 47);
+            this.rbTang.Name = "rbTang";
+            this.rbTang.Size = new System.Drawing.Size(71, 17);
+            this.rbTang.TabIndex = 4;
+            this.rbTang.Text = "Tăng dần";
+            this.rbTang.UseVisualStyleBackColor = true;
+            // 
+            // rbMin
+            // 
+            this.rbMin.AutoSize = true;
+            this.rbMin.Enabled = false;
+            this.rbMin.Location = new System.Drawing.Point(90, 74);
+            this.rbMin.Name = "rbMin";
+            this.rbMin.Size = new System.Drawing.Size(74, 17);
+            this.rbMin.TabIndex = 7;
+            this.rbMin.TabStop = true;
+            this.rbMin.Text = "Thấp nhất";
+            this.rbMin.UseVisualStyleBackColor = true;
+            // 
+            // rbGiam
+            // 
+            this.rbGiam.AutoSize = true;
+            this.rbGiam.Enabled = false;
+            this.rbGiam.Location = new System.Drawing.Point(90, 46);
+            this.rbGiam.Name = "rbGiam";
+            this.rbGiam.Size = new System.Drawing.Size(70, 17);
+            this.rbGiam.TabIndex = 5;
+            this.rbGiam.Text = "Giảm dần";
+            this.rbGiam.UseVisualStyleBackColor = true;
+            // 
+            // cbLoaiSX
+            // 
+            this.cbLoaiSX.FormattingEnabled = true;
+            this.cbLoaiSX.Items.AddRange(new object[] {
+            "Số lượng",
+            "Doanh thu"});
+            this.cbLoaiSX.Location = new System.Drawing.Point(37, 16);
+            this.cbLoaiSX.Name = "cbLoaiSX";
+            this.cbLoaiSX.Size = new System.Drawing.Size(132, 21);
+            this.cbLoaiSX.TabIndex = 3;
+            this.cbLoaiSX.TextChanged += new System.EventHandler(this.cbLoaiSX_TextChanged);
+            // 
             // ThongKeNhanvien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.cbThang);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbNam);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbTuoiden);
@@ -122,6 +276,8 @@ namespace QuanLyCuaHangBanDienThoai
             this.Text = "Thống kê Nhân Viên theo tuổi";
             this.Load += new System.EventHandler(this.ThongKeNhanvien_Load);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,5 +292,16 @@ namespace QuanLyCuaHangBanDienThoai
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbNam;
+        private System.Windows.Forms.ComboBox cbThang;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbMax;
+        private System.Windows.Forms.RadioButton rbTang;
+        private System.Windows.Forms.RadioButton rbMin;
+        private System.Windows.Forms.RadioButton rbGiam;
+        private System.Windows.Forms.ComboBox cbLoaiSX;
     }
 }
--- QuanLyCuaHangBanDienThoai/QuanLyCuaHangBanDienThoai/ThongKeNhanvien.cs
+++ QuanLyCuaHangBanDienThoai/QuanLyCuaHangBanDienThoai/ThongKeNhanvien.cs
@@ -20,23 +20,14 @@ namespace QuanLyCuaHangBanDienThoai
     {
         String constr = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
         bool test = true;
-
+        String TKNV = @"..\..\CrytalReport\ThongkeNV.rpt";
+        String SXTKNV = @"..\..\CrytalReport\SXTKNV.rpt";
+        String SXTKNVtheoNam = @"..\..\CrytalReport\SXTKNVtheoNam.rpt";
         public ThongKeNhanvien()
         {
             InitializeComponent();
         }
-
-        private void ThongKeNhanvien_Load(object sender, EventArgs e)
-        {
-            // sapxep();
-            ReportDocument rp = new ReportDocument();
-            String path = Path.GetFullPath(@"..\..\CrytalReport\ThongkeTuoiNV.rpt");
-            rp.Load(path);
-
-            crvNV.ReportSource = rp;
-            crvNV.Refresh();
-        }
-
+        //Load năm trong cbNăm
         public DataTable findAllTKNV()
         {
             using (SqlConnection cnn = new SqlConnection(constr))
@@ -51,6 +42,80 @@ namespace QuanLyCuaHangBanDienThoai
                             ad.Fill(dt);
                             return dt;
                         }
+                    }
+                }
+            }
+        }
+        public void loadYearCombox(ComboBox cb)
+        {
+            DataTable dtProducer = findAllTKNV();
+            DataView v = new DataView(dtProducer);
+            v.Sort = "Năm";
+
+            cb.DataSource = v;
+            cb.DisplayMember = "Năm";
+            cb.ValueMember = "Năm";
+        }
+        private void ThongKeNhanvien_Load(object sender, EventArgs e)
+        {
+            // sapxep();
+            loadYearCombox(cbNam);
+            ReportDocument rp = new ReportDocument();
+            String path = Path.GetFullPath(@"..\..\CrytalReport\ThongkeTuoiNV.rpt");
+            rp.Load(path);
+
+            crvNV.ReportSource = rp;
+            crvNV.Refresh();
+        }
+
+
+        private void SXNV(string kieu, string loai)
+        {
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                String sql = "select * from TKNV WHERE [Năm] like '%" + cbNam.Text + "%' and[Tháng]like'%" + cbThang.Text + "%'order by[" + loai + "] " + kieu;
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                    {
+                        DataSet2 ds = new DataSet2();
+                        DataTable dt = new DataTable();
+                        ad.Fill(ds);
+                        //ThongKeDienThoai rpt = new ThongKeDienThoai();
+                        ReportDocument rpt = new ReportDocument();
+                        String path = Path.GetFullPath(SXTKNV);
+                        rpt.Load(path);
+                        rpt.SetDataSource(ds.Tables[1]);
+                        crvNV.ReportSource = rpt;
+                        crvNV.Refresh();
+                    }
+                }
+            }
+        }
+
+        private void MinmaxNV(string mm, string loai, string thang)
+        {
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                String sql = "select [Mã NV],[Tên NV],[Tuổi],[SLĐT bán],[Doanh thu],TKNV.[Tháng],TKNV.[Năm] from TKNV, (select[Tháng],[Năm], " + mm + "([" + loai + "]) as maxtin from TKNV group by [Năm], [Tháng])a where TKNV.Tháng = " + thang + " and [" + loai + "] = a.maxtin and TKNV.[Năm] like'%" + cbNam.Text + "%' GROUP BY [Mã NV],[Tên NV],[Tuổi],[SLĐT bán],[Doanh thu],TKNV.[Tháng],TKNV.[Năm]";
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    // MessageBox.Show("select [Mã NV],[Tên NV],[Tuổi],[SLĐT bán],[Doanh thu],TKNV.[Tháng],TKNV.[Năm] from TKNV, (select[Tháng],[Năm], a.Tháng ([" + loai + "]) as maxtin from TKNV group by [Năm], [Tháng])a where TKNV.Tháng = " + thang + " and [" + loai + "] = a.maxtin and TKNV.[Năm] like'%" + cbNam.Text + "%'");
+
+                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                    {
+                        DataSet2 ds = new DataSet2();
+                        DataTable dt = new DataTable();
+                        ad.Fill(ds);
+                        //ThongKeDienThoai rpt = new ThongKeDienThoai();
+                        ReportDocument rpt = new ReportDocument();
+                        String path = Path.GetFullPath(SXTKNV);
+                        rpt.Load(path);
+                        rpt.SetDataSource(ds.Tables[1]);
+                        crvNV.ReportSource = rpt;
+                        crvNV.Refresh();
                     }
                 }
             }
@@ -79,7 +144,78 @@ namespace QuanLyCuaHangBanDienThoai
                 }
             }
         }
-
+        private void TKNVtheoNam()
+        {
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                String sql = "select   [Mã NV], [Tên NV],[Tuổi],sum ([SLĐT bán])as [SLĐT bán],sum([Doanh thu]) as [Doanh thu], [Năm] from TKNV where [Năm] like N'" + cbNam.Text + "' group by[Mã NV], [Tên NV],[Tuổi],[Năm]  ";
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                    {
+                        DataSet5 ds = new DataSet5();
+                        DataTable dt = new DataTable();
+                        ad.Fill(ds);
+                        //ThongKeDienThoai rpt = new ThongKeDienThoai();
+                        ReportDocument rpt = new ReportDocument();
+                        String path = Path.GetFullPath(SXTKNVtheoNam);
+                        rpt.Load(path);
+                        rpt.SetDataSource(ds.Tables[1]);
+                        crvNV.ReportSource = rpt;
+                        crvNV.Refresh();
+                    }
+                }
+            }
+        }
+        private void SXNVtheonam(string kieu, string loai)
+        {
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                String sql = "select   [Mã NV], [Tên NV],[Tuổi],sum ([SLĐT bán])as [SLĐT bán],sum([Doanh thu]) as [Doanh thu], [Năm] from TKNV where [Năm] like N'" + cbNam.Text + "' group by[Mã NV], [Tên NV],[Tuổi],[Năm] order by sum([" + loai + "]) " + kieu;
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                    {
+                        DataSet5 ds = new DataSet5();
+                        DataTable dt = new DataTable();
+                        ad.Fill(ds);
+                        //ThongKeDienThoai rpt = new ThongKeDienThoai();
+                        ReportDocument rpt = new ReportDocument();
+                        String path = Path.GetFullPath(SXTKNVtheoNam);
+                        rpt.Load(path);
+                        rpt.SetDataSource(ds.Tables[1]);
+                        crvNV.ReportSource = rpt;
+                        crvNV.Refresh();
+                    }
+                }
+            }
+        }
+        private void MinmaxNVtheoNam(string kieu, string loai)
+        {
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                String sql = "select top(1)  [Mã NV], [Tên NV],[Tuổi],sum ([SLĐT bán])as [SLĐT bán],sum([Doanh thu]) as [Doanh thu], [Năm] from TKNV where [Năm] like N'" + cbNam.Text + "' group by[Mã NV], [Tên NV],[Tuổi],[Năm] order by sum([" + loai + "]) " + kieu;
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                    {
+                        DataSet5 ds = new DataSet5();
+                        DataTable dt = new DataTable();
+                        ad.Fill(ds);
+                        //ThongKeDienThoai rpt = new ThongKeDienThoai();
+                        ReportDocument rpt = new ReportDocument();
+                        String path = Path.GetFullPath(SXTKNVtheoNam);
+                        rpt.Load(path);
+                        rpt.SetDataSource(ds.Tables[1]);
+                        crvNV.ReportSource = rpt;
+                        crvNV.Refresh();
+                    }
+                }
+            }
+        }
         private void loctuoi(string lenh)
         {
             ReportDocument rp = new ReportDocument();
@@ -89,7 +225,180 @@ namespace QuanLyCuaHangBanDienThoai
             crvNV.ReportSource = rp;
             crvNV.Refresh();
         }
+        private void locnamthang(string lenh, string ten)
+        {
+            ReportDocument rp = new ReportDocument();
+            String path = Path.GetFullPath(ten);
+            rp.Load(path);
+            rp.RecordSelectionFormula = lenh;
+            crvNV.ReportSource = rp;
+            crvNV.Refresh();
+        }
+        private void loc()
+        {
+            if (cbNam.Text != "")//năm
+            {
+                if (cbThang.Text != "")//năm+tháng
+                {
+                    if (cbLoaiSX.Text == "")//năm+tháng +loaisx
+                    {
+                        if (!rbTang.Checked && !rbGiam.Checked && !rbMin.Checked && !rbMax.Checked)//không có kiểu sx
+                        {
 
+
+                            locnamthang("{@Nam}=" + cbNam.Text + " and {@Thang}=" + cbThang.Text, TKNV);
+
+                        }
+                        else
+                            MessageBox.Show("Mời bạn nhập kiểu sắp xếp");
+                    }
+                    else
+                    {
+                        if (!rbTang.Checked && !rbGiam.Checked && !rbMin.Checked && !rbMax.Checked)
+                            MessageBox.Show("Mời bạn chọn kiểu sắp xếp");
+                        if (cbLoaiSX.Text == "Số lượng")
+                        {
+                            if (rbGiam.Checked)
+                            {
+                                SXNV("desc", "SLĐT bán");
+                            }
+                            else if (rbTang.Checked)
+                            {
+
+
+                                SXNV("asc", "SLĐT bán");
+
+                            }
+                            else if (rbMax.Checked)
+                            {
+
+                                MinmaxNV("Max", "SLĐT bán", cbThang.Text);
+
+                            }
+                            else if (rbMin.Checked)
+                            {
+
+                                MinmaxNV("Min", "SLĐT bán", cbThang.Text);
+
+                            }
+                        }
+                        if (cbLoaiSX.Text == "Doanh thu")
+                        {
+                            if (rbTang.Checked)
+                            {
+                                // sapxep("asc", ten2, loai2, i, tenv);
+
+                                SXNV("asc", "Doanh thu");
+
+                            }
+                            else if (rbGiam.Checked)
+                            {
+
+                                SXNV("desc", "Doanh thu");
+
+                                //MessageBox.Show("ok");
+                            }
+                            else if (rbMax.Checked)
+                            {
+
+                                MinmaxNV("Max", "Doanh thu", cbThang.Text);
+
+                            }
+                            else if (rbMin.Checked)
+                            {
+
+                                MinmaxNV("Min", "Doanh thu", cbThang.Text);
+
+                            }
+                        }
+                    }
+                }
+                else//năm thôi
+                {
+                    if (cbLoaiSX.Text == "")
+                    {
+                        if (!rbTang.Checked && !rbGiam.Checked && !rbMin.Checked && !rbMax.Checked)
+                        {
+                            MessageBox.Show("Lọc mỗi năm");
+                            TKNVtheoNam();
+
+                        }
+                        else
+                            MessageBox.Show("Mời bạn nhập loại sắp xếp");
+                    }
+                    else
+                    {
+                        if (!rbTang.Checked && !rbGiam.Checked && !rbMin.Checked && !rbMax.Checked)
+                            MessageBox.Show("Mời bạn chọn kiểu sắp xếp");
+                        if (cbLoaiSX.Text == "Số lượng")
+                        {
+                            if (rbGiam.Checked)
+                            {
+
+                                SXNVtheonam("desc", "SLĐT bán");
+
+                            }
+                            else if (rbTang.Checked)
+                            {
+                                // sapxep("asc", ten2, loai1, i, tenv);
+
+                                SXNVtheonam("asc", "SLĐT bán");
+
+                            }
+                            else if (rbMax.Checked)
+                            {
+
+
+                                MinmaxNVtheoNam("desc", "SLĐT bán");
+
+                            }
+                            else if (rbMin.Checked)
+                            {
+
+                                MinmaxNVtheoNam("asc", "SLĐT bán");
+
+                            }
+                        }
+                        if (cbLoaiSX.Text == "Doanh thu")
+                        {
+                            if (rbTang.Checked)
+                            {
+
+                                SXNVtheonam("asc", "Doanh thu");
+
+                            }
+                            else if (rbGiam.Checked)
+                            {
+
+                                SXNVtheonam("desc", "Doanh thu");
+
+                            }
+                            else if (rbMax.Checked)
+                            {
+
+                                MinmaxNVtheoNam("desc", "Doanh thu");
+
+                            }
+                            else if (rbMin.Checked)
+                            {
+
+                                MinmaxNVtheoNam("asc", "Doanh thu");
+
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ReportDocument rp = new ReportDocument();
+                String path = Path.GetFullPath(TKNV);
+                rp.Load(path);
+
+                crvNV.ReportSource = rp;
+                crvNV.Refresh();
+            }
+        }
         private void btnHien_Click(object sender, EventArgs e)
         {
             if (test == true)
@@ -108,7 +417,8 @@ namespace QuanLyCuaHangBanDienThoai
                 else if (tbTuoitu.Text != "" && tbTuoiden.Text == "")
                 {
                     loctuoi("{@Tuoi}>= " + tbTuoitu.Text);
-                    MessageBox.Show("{@Tuoi}>= " + tbTuoitu.Text);
+                   
+
                 }
                 else if (tbTuoitu.Text == "" && tbTuoiden.Text != "")
                 {
@@ -120,6 +430,8 @@ namespace QuanLyCuaHangBanDienThoai
                     // sapxep();
                 }
             }
+           // loc();
+
         }
 
         private void tbTuoitu_Validating(object sender, CancelEventArgs e)
@@ -165,6 +477,28 @@ namespace QuanLyCuaHangBanDienThoai
                 //errorProvider1.SetError(tbTuoiden, "");
                 errorProvider1.Clear();
                 test = true;
+            }
+        }
+
+        private void cbLoaiSX_TextChanged(object sender, EventArgs e)
+        {
+            if (cbLoaiSX.Text != "")
+            {
+                rbTang.Enabled = true;
+                rbGiam.Enabled = true;
+                rbMax.Enabled = true;
+                rbMin.Enabled = true;
+
+                //gbTheo.Enabled = true;
+            }
+            else
+            {
+                rbTang.Enabled = false;
+                rbGiam.Enabled = false;
+                rbMax.Enabled = false;
+                rbMin.Enabled = false;
+
+                //gbTheo.Enabled = false;
             }
         }
     }
